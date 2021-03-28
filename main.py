e6e26bc4cab315f18834cb69dfc504e5b987833d
trl242 <royaurallockhart@gmail.com>
2021-03-28 15:05:33 -0700

    updates currency conversion, needs to take input to create now value

--- main.py
+++ main.py
@@ -34,13 +34,12 @@ my_notebook.tab(1, state='disabled')
 # CURRENCY STUFF
 #######################
 def lock():
-	if not home_entry.get() or not conversion_entry.get() or not rate_entry.get():
+	if not home_entry.get() or not conversion_entry.get():
 		messagebox.showwarning("WARNING!", "You Didn't Fill Out All The Fields")
 	else:
 		# Disable entry boxes
 		home_entry.config(state="disabled")
 		conversion_entry.config(state="disabled")
-		rate_entry.config(state="disabled")
 		# Enable tab
 		my_notebook.tab(1, state='normal')
 		# Change Tab Field
@@ -54,7 +53,21 @@ def unlock():
 	rate_entry.config(state="normal")
 	# Disable Tab
 	my_notebook.tab(1, state='disabled')
-home = LabelFrame(currency_frame, text="Name of Initial Currency")
+
+def converter():
+	currency_1 = str(home_entry.get())
+	currency_2 = str(conversion_entry.get())
+	try:
+	# Live currency update
+		conversion_rate = (c.convert(1, currency_1, currency_2))
+		# conversion_rate = c.convert(1, 'USD', 'CAD')
+		# print(conversion_rate)
+		rate_entry.config(text=conversion_rate)
+	except:
+		Print("Entry not working")
+		pass
+
+home = LabelFrame(currency_frame, text="Symbol of Initial Currency")
 home.pack(pady=20)
 
 # Home currency entry box
@@ -66,7 +79,7 @@ conversion = LabelFrame(currency_frame, text="Conversion Currency")
 conversion.pack(pady=20)
 
 # convert to label
-conversion_label = Label(conversion, text="Currency To Convert To...")
+conversion_label = Label(conversion, text="Symbol of Currency To Convert To...")
 conversion_label.pack(pady=10)
 
 # Convert To Entry
@@ -78,7 +91,7 @@ rate_label = Label(conversion, text="Current Conversion Rate...")
 rate_label.pack(pady=10)
 
 # Rate To Entry
-rate_entry = Entry(conversion, font=("Helvetica", 24))
+rate_entry = Label(conversion, text="Click Conversion Rate Button", font=("Helvetica", 24))
 rate_entry.pack(pady=10, padx=10)
 
 # Button Frame
@@ -87,21 +100,21 @@ button_frame.pack(pady=20)
 
 # Create Buttons
 lock_button = Button(button_frame, text="Lock", command=lock)
-lock_button.grid(row=0, column=0, padx=10)
+lock_button.grid(row=0, column=1, padx=10)
 
 unlock_button = Button(button_frame, text="Unlock", command=unlock)
-unlock_button.grid(row=0, column=1, padx=10)
+unlock_button.grid(row=0, column=2, padx=10)
 
 
 #######################
 # CONVERSION STUFF
 #######################
 def convert():
+	global conversion_rate
 	# Clear Converted Entry Box
 	converted_entry.delete(0, END)
-
 	# Convert
-	conversion = float(rate_entry.get()) * float(amount_entry.get())
+	conversion = float(conversion_rate) * float(amount_entry.get())
 	# Convert to two decimals
 	conversion = round(conversion,2)
 	# Add commas
@@ -112,14 +125,9 @@ def clear():
 	amount_entry.delete(0, END)
 	converted_entry.delete(0, END)
 
-def converter(home_entry,conversion_entry):
-	try:
-	# Live currency update
-		conversion_rate = c.convert(1, {home_entry}, {conversion_entry})
-	# 	conversion_rate = c.convert(1, 'USD', 'CAD')
-		print(conversion_rate)
-	except:
-		pass
+#convertion rate button
+calculate_rate_button = Button(button_frame, text="Conversion Rate", command=converter)
+calculate_rate_button.grid(row=0, column=0, padx=10)
 
 amount_label = LabelFrame(conversion_frame, text="Amount To Conver")
 amount_label.pack(pady=20)
